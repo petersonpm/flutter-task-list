d1280db5aa2e9763b9879cfcb9897cf29e04a8d6
Peterson Macedo <petersonmacedo.pm@gmail.com>
2023-06-16 15:52:21 -0400

    Troca de icone e fundo do app

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -37,101 +37,116 @@ class _TodoListPageState extends State<TodoListPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: todoController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Adicione uma Tarefa',
-                          hintText: 'EX. Estudar para a prova',
-                            errorText: errorText,
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Color(0xffDC143C),
-                                width:2,
-                              ),
-                            ),
-                          labelStyle: const TextStyle(
-                            color: Color(0xffDC143C),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    ElevatedButton(
-                      onPressed: () {
-                        String text = todoController.text;
+      child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/image.jpg'),
+              fit: BoxFit.cover,
 
-                        if(text.isEmpty){
-                          setState(() {
-                            errorText = 'O titulo nao pode ser vazio!';
-                          });
-                          return;
-                        }
-
-                        setState(() {
-                          Todo newTodo =
-                              Todo(title: text, dateTime: DateTime.now());
-                          todos.add(newTodo);
-                          errorText = null;
-                        });
-                        todoController.clear();
-                        todoRepository.saveTodoList(todos);
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xffDC143C),
-                          padding: const EdgeInsets.all(8)),
-                      child: const Icon(
-                        Icons.add,
-                        size: 45,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Flexible(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      for (Todo todo in todos)
-                        TodoListItem(
-                          todo: todo,
-                          onDelete: onDelete,
-                        ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Você Possui ${todos.length} Tarefas Pendentes',
-                      ),
-                    ),
-                    const SizedBox(width: 8),
-                    ElevatedButton(
-                      onPressed: showDeleteTodosConfirmationDialog,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xffDC143C),
-                          padding: const EdgeInsets.all(16)),
-                      child: const Text('Limpar tudo'),
-                    ),
-                  ],
-                ),
-              ],
             ),
           ),
-        ),
+           child: Scaffold(
+             backgroundColor: Colors.transparent,
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: TextField(
+                                controller: todoController,
+                                decoration: InputDecoration(
+                                  border: const OutlineInputBorder(),
+                                  labelText: 'Adicione uma Tarefa',
+                                  hintText: 'EX. Estudar para a prova',
+                                  errorText: errorText,
+                                  focusedBorder: const OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Color(0xffDC143C),
+                                      width:2,
+                                    ),
+                                  ),
+                                  labelStyle: const TextStyle(
+                                    color: Color(0xffDC143C),
+                                  ),
+                                ),
+                              )
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        ElevatedButton(
+                          onPressed: () {
+                            String text = todoController.text;
+
+                            if(text.isEmpty){
+                              setState(() {
+                                errorText = 'O titulo não pode ser vazio!';
+                              });
+                              return;
+                            }
+
+                            setState(() {
+                              Todo newTodo =
+                              Todo(title: text, dateTime: DateTime.now());
+                              todos.add(newTodo);
+                              errorText = null;
+                            });
+                            todoController.clear();
+                            todoRepository.saveTodoList(todos);
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xffDC143C),
+                              padding: const EdgeInsets.all(8)),
+                          child: const Icon(
+                            Icons.add,
+                            size: 45,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    Flexible(
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: [
+                          for (Todo todo in todos)
+                            TodoListItem(
+                              todo: todo,
+                              onDelete: onDelete,
+                            ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Você Possui ${todos.length} Tarefas Pendentes',
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        ElevatedButton(
+                          onPressed: showDeleteTodosConfirmationDialog,
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xffDC143C),
+                              padding: const EdgeInsets.all(16)),
+                          child: const Text('Limpar tudo'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
       ),
     );
   }
@@ -150,12 +165,12 @@ class _TodoListPageState extends State<TodoListPage> {
       SnackBar(
         content: Text(
           'Tarefa ${todo.title} foi Removida com sucesso!',
-          style: TextStyle(fontSize: 12),
+          style: const TextStyle(fontSize: 12),
         ),
         backgroundColor: Color(0xffDC143C),
         action: SnackBarAction(
           label: 'Desfazer',
-          textColor: Color(0xffDC143C),
+          textColor: const Color(0xffDC143C),
           onPressed: () {
             setState(() {
               todos.insert(deletedTodoPos!, deletedTodo!);
@@ -173,15 +188,15 @@ class _TodoListPageState extends State<TodoListPage> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('Limpar tudo?'),
-        content: Text('Você tem certeza que deseja apagar todas as tarefas?'),
+        title: const Text('Limpar tudo?'),
+        content: const Text('Você tem certeza que deseja apagar todas as tarefas?'),
         actions: [
           TextButton(
             onPressed: () {
               Navigator.of(context).pop();
             },
             style: TextButton.styleFrom(foregroundColor: Colors.black),
-            child: Text('Cancelar'),
+            child: const Text('Cancelar'),
           ),
           TextButton(
             onPressed: () {
@@ -189,7 +204,7 @@ class _TodoListPageState extends State<TodoListPage> {
               deleteAllTodos();
             },
             style: TextButton.styleFrom(foregroundColor: Color(0xffDC143C)),
-            child: Text('Limpar Tudo!'),
+            child: const Text('Limpar Tudo!'),
           ),
         ],
       ),
